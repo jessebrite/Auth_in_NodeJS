27ca312fd51efaab009090929c29faa2facac65e
jessebrite <jessebrite@gmail.com>
2019-05-18 20:08:01 +0100

    Google auth implemented

---
+++ config/auth.js
@@ -0,0 +1,10 @@
+module.exports = {
+	// Google auth
+	'googleAuth' : {
+    'clientID'      : '403499052685-6kocp4poer25rf9bc6eprko825b2v2uj.apps.googleusercontent.com',
+    'clientSecret'  : 't7ZOmoaWGlTYbZvr2EGgjrUe',
+    'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+    // 'profileFields' : ['id', 'email', 'name']
+	}
+
+}
